f856f7e988bdfe5a92fb0093d3817df601c647ab
Alexey Porotnikov <alexey.porotnikov@gmail.com>
2015-07-12 13:49:28 +0300

    test for json type

--- test/data.jl
+++ test/data.jl
@@ -57,6 +57,20 @@ function test_bytea()
     end
 end
 
+function test_json()
+    typ = PostgreSQL.PostgresType{:json}
+    dict1 = Dict{String,Any}({"bobr dobr" => [1, 2, 3]})
+    p = PostgreSQL.pgdata(typ, convert(Ptr{Uint8}, C_NULL), dict1)
+    try
+        dict2 = PostgreSQL.jldata(typ, p)
+        @test typeof(dict1) == typeof(dict2)
+        @test dict1 == dict2
+    finally
+        c_free(p)
+    end
+end
+
 test_numerics()
 test_strings()
 test_bytea()
+test_json()
